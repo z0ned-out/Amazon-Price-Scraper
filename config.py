e8375a2e7cd802660468a367a5aa30a45dc855dd
z0ned-out <ashukhanna94@gmail.com>
2022-07-06 07:05:30 +0530

    Add files via upload
    For storing configs/urls

---
+++ config.py
@@ -0,0 +1,15 @@
+# specify discord webhook url below
+webhook_url = "https://discord.com/api/webhooks/993701834155900938/yv-aLBqUKuZKeaLud8tCQu7lrcFak8ig7Z9Z6BYQ2Ew-EjiVPqKZa1q4NYNTgHLtgCXM"
+
+
+#specify amazon product url below
+product_url = "https://www.amazon.in/gp/product/B08N5XSG8Z/ref=ewc_pr_img_1?smid=A14CZOWI0VEHLG&psc=1"
+
+#specify user agent below. can be traced via google
+HEADERS = {"User-Agent":"Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/103.0.5060.114 Safari/537.36"}
+
+#specify thumbnail url
+thumbnail_url = 'https://store.storeimages.cdn-apple.com/4668/as-images.apple.com/is/macbook-air-gallery3-20201110?wid=2000&hei=1536&fmt=jpeg&qlt=95&.v=1632937845000'
+
+#specify icon url
+icon_url = "https://c.tenor.com/8vSJsVW-1pQAAAAj/police-car-light-joypixels.gif"
