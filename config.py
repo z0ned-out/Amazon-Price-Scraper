f0cfc0a4bc50b2ed4e20ac3e34d22887287b91e2
z0ned-out <ashukhanna94@gmail.com>
2022-07-07 07:59:33 +0530

    Update config.py

--- config.py
+++ config.py
@@ -9,7 +9,7 @@ product_url = "https://www.amazon.in/gp/product/B08N5XSG8Z/ref=ewc_pr_img_1?smid
 x = 92900
 
 #specify user agent below. can be traced via google
-HEADERS = {"User-Agent":"Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/103.0.5060.114 Safari/537.36"}
+HEADERS = {"User-Agent":"Mention user agent here"}
 
 #specify thumbnail url
 thumbnail_url = 'https://store.storeimages.cdn-apple.com/4668/as-images.apple.com/is/macbook-air-gallery3-20201110?wid=2000&hei=1536&fmt=jpeg&qlt=95&.v=1632937845000'
